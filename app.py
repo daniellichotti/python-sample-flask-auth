20ca1c8f08f03ef2d6a7c6390a577846605ba80c
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-11 02:34:41 -0300

    Autenticação e FlaskLogin - Criando Views de Logout e Cadastro de Usuário - parte 2

--- app.py
+++ app.py
@@ -40,6 +40,19 @@ def logout():
     logout_user()
     return jsonify({'message': 'Logout realizado com sucesso'})
 
+@app.route('/user', methods=['POST'])
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({'message': 'Usuario cadastrado com sucesso'})
+
+    return jsonify({'message': 'Dados invalidos'}), 400
 
 @app.route('/hello-world')
 def hello_world():
