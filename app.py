20887e1e8266b9ea2a2051850be8d8ad148cfd05
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-11 01:21:48 -0300

    Banco de dados e SQLAlchemy - Modelando o Usuário

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
 db = SQLAlchemy(app)
+
+user = {
+    
+}
 
 @app.route('/hello-world')
 def hello_world():
