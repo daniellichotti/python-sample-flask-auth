6d887c18826cf970f424897906bf63f5e762490c
Daniel Lichotti <danielclichotti@gmail.com>
2024-04-11 02:56:43 -0300

    Autenticação e FlaskLogin - Rotas de Read e Update do Usuario

--- app.py
+++ app.py
@@ -54,6 +54,39 @@ def create_user():
 
     return jsonify({'message': 'Dados invalidos'}), 400
 
+@app.route('/user/<int:id_user>', methods=['GET'])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
+
+    if user:
+        return {"username": user.username}
+    return jsonify({'message': 'Usuario não encontrado'}), 404
+
+@app.route('/user/<int:id_user>', methods=['PUT'])
+@login_required
+def update_user(id_user):
+    data = request.json
+    user = User.query.get(id_user) #recuperar usuario pelo id
+
+    if user and data.get('password'):
+        user.password = data.get("password")
+        db.session.commit()
+
+        return jsonify({'message': f'Usuario {id_user} atualizado com sucesso'})
+    return jsonify({'message': 'Usuario não encontrado'}), 404 
+
+@app.route('/user/<int:id_user>', methods=['DELETE'])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
+
+    if user:
+        return jsonify({'message': 'Usuario deletado com suce'})
+    
+    return jsonify({'message': 'Usuario não encontrado'}), 404
+
+
 @app.route('/hello-world')
 def hello_world():
     return 'Hello World!'
